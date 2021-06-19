fa6af1a069693bb00e8debff7afe913925b06313
tungcnn <sontung.cnn@gmail.com>
2021-06-19 15:25:32 +0700

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { DashboardMenuComponent } from './components/dashboard-menu/dashboard-menu.component';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../environments/environment';
+import {AngularFireStorageModule} from '@angular/fire/storage';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
 
 @NgModule({
   declarations: [
@@ -22,7 +26,10 @@ import {DashboardComponent} from './components/dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireStorageModule,
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
