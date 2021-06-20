5a606611ba3fd449796b6d107eb85bedf5154277
voduyquocthai <voduyquocthai@gmail.com>
2021-06-20 13:19:13 +0700

    done logout

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,11 +1,12 @@
 import {EventEmitter, Injectable, Output} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {SignupRequestPayload} from "../components/header/signup-request.payload";
-import {Observable} from "rxjs";
+import {Observable, throwError} from "rxjs";
 import {environment} from '../../environments/environment';
 import {LoginRequestPayload} from "../components/header/login-request.payload";
 import {LoginResponse} from "../components/header/login-response.payload";
 import {map} from "rxjs/operators";
+import {User} from "../model/user";
 
 const API_URL = `${environment.apiUrl}`;
 
@@ -39,10 +40,29 @@ export class AuthService {
   }
 
   getUserName() {
-    return window.localStorage.retrieve('username');
+    return window.localStorage.getItem('username');
   }
 
   getUserId() {
-    return window.localStorage.retrieve('userId');
+    return parseInt(window.localStorage.getItem('userId')) ;
+  }
+
+  isLoggedIn(){
+    let username: string =  this.getUserName();
+    return username != null;
+  }
+
+  logout() {
+    this.httpClient.post(`${API_URL}/auth/logout`, null).subscribe(data => {
+        console.log(data);
+      }, error => {
+        throwError(error);
+      });
+    window.localStorage.removeItem('userId')
+    window.localStorage.removeItem('username')
+  }
+
+  getUserByUserName(username: string): Observable<User> {
+    return this.httpClient.get<User>(`${API_URL}/users/${username}`);
   }
 }
